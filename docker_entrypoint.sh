d82db294373bd062612feb363be43a2b1b6fc1e4
haroon <haroon@bu.edu>
2025-07-17 16:37:45 -0400

    working entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,4 +6,5 @@ echo "[Docker Entrypoint] Pulling latest code..."
 git pull || true  # allow fail if not a git repo
 
 exec python3 exts/customView/customView/pyqt_interface.py "$@"
+#cd .. && ./isaac-sim.sh "$@"
 #  
